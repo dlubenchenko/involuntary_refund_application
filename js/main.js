3e8fb08d5a8f36e27f52256aacc5f7516ff2dbe0
dlubenchenko <d.lubenchenko@gmail.com>
2021-06-14 19:01:25 +0300

    selection

--- js/main.js
+++ js/main.js
@@ -1,226 +1,49 @@
-function findFp() {
-    for (let j = 0; j < document.getElementsByClassName('fp').length; j++) {
-        if (document.getElementById('fp').value === 'FP CASH') {
-            document.getElementsByClassName('cash')[0].style.display = 'grid'
-            document.getElementsByClassName('cc')[0].style.display = 'none'
-        } else if (document.getElementById('fp').value === 'FP CC') {
-            document.getElementsByClassName('cc')[0].style.display = 'grid'
-            document.getElementsByClassName('cash')[0].style.display = 'none'
-        } else if (document.getElementById('fp').value === 'FP CC + FP CASH') {
-            document.getElementsByClassName('cc')[0].style.display = 'grid'
-            document.getElementsByClassName('cash')[0].style.display = 'grid'
-        } else {
-            document.getElementsByClassName('cc')[0].style.display = 'none'
-            document.getElementsByClassName('cash')[0].style.display = 'none'
-        }
-    }
+let formSelect = document.querySelector('.form-select')
+let selected = document.querySelector('.form-select')
+let secondBlock = document.querySelector('.second-block')
+let options = {
+	select: ['Парсер', 'Повернення', 'Переоформлення', 'Інфо по АК'],
+	0: ['Kiwi', 'GloryHoliday', 'FlyArystan', 'S7', 'SkyUP', 'TUI'],
+	1: ['Повне', 'Часткове'],
+	2: ['Amadeus', 'Sabre'],
+	3: [
+		'Зміна часу',
+		'Клас',
+		'Лист очік.',
+		'Інтервал дат',
+		'Code-share',
+		'Дод. Авториз.',
+	],
 }
 
-function option() {
+let block = `
+	<div class="btn-toolbar" role="toolbar">
+		<div class="btn-group top-0 start-50 translate-middle mt-5 mb-0 btn-group delete" role="group">
+		</div>
+	</div>`
 
-    const numOfTkt = document.querySelectorAll('.ticketnumber')
-    const numOfNewTkt = document.querySelectorAll('.newticketnumber')
-    const select = document.querySelectorAll('.refund')
-    const emd = document.querySelectorAll('.emdnumber')
+secondBlock.insertAdjacentHTML('afterbegin', block)
+let btnGroup = document.querySelector('.btn-group')
 
+options.select.forEach((e, i) => {
+	formSelect.insertAdjacentHTML(
+		'beforeend',
+		`<option value="${i}">${e}</option>`
+	)
+})
 
-    select.forEach((key) => {
-        if (key.className.indexOf(document.itemsCheck.refundType.value) != -1) {
-            key.classList.add('refund-active')
-            numOfNewTkt.forEach((ntkey) => {
-                if ('refund2 refund3 refund4 refund6 refund7'.indexOf(document.itemsCheck.refundType.value) != -1) {
-                    if (ntkey.className.indexOf(document.itemsCheck.tktNumList.value) != -1) {
-                        ntkey.classList.add('ticket-active')
-                        document.getElementById('newticketnumber').classList.add('refund-active')
-                        document.getElementById('newticketdoi').classList.add('refund-active')
-                    } else {
-                        ntkey.classList.remove('ticket-active')
-                        document.getElementById('newticketnumber').classList.remove('refund-active')
-                        document.getElementById('newticketdoi').classList.remove('refund-active')
-                    }
-                }
-            })
-            numOfTkt.forEach((tkey) => {
-                if (tkey.className.indexOf(document.itemsCheck.tktNumList.value) != -1) {
-                    tkey.classList.add('ticket-active')
-                    document.getElementById('ticketnumber').classList.add('refund-active')
-                    document.getElementById('ticketdoi').classList.add('refund-active')
-                } else {
-                    tkey.classList.remove('ticket-active')
-                    document.getElementById('ticketnumber').classList.remove('refund-active')
-                    document.getElementById('ticketdoi').classList.remove('refund-active')
-                }
-            })
-            if (document.querySelectorAll('.emdnumber')[1].className.indexOf(document.itemsCheck.emdNumList.value) != -1) {
-                document.getElementById('emdnumber').classList.add('refund-active')
-                document.getElementById('emddoi').classList.add('refund-active')
-                emd.forEach((key) => {
-                    if (key.className.indexOf(document.itemsCheck.emdNumList.value) != -1) {
-                        key.classList.add('ticket-active')
-                    } else {
-                        key.classList.remove('ticket-active')
-                    }
-                })
-            } else {
-                document.getElementById('emdnumber').classList.remove('refund-active')
-                document.getElementById('emddoi').classList.remove('refund-active')
-            }
-        } else {
-            key.classList.remove('refund-active')
-        }
-    })
+selected.addEventListener('change', () => {
+	choiceFilter(btnGroup)
+	options[selected.value].forEach((e) => {
+		btnGroup.insertAdjacentHTML(
+			'afterbegin',
+			`<button type="button" class="btn ${selected.value} btn-outline-secondary">${e}</button>`
+		)
+	})
+})
+
+let choiceFilter = (choice) => {
+	while (choice.firstChild) {
+		choice.removeChild(choice.firstChild)
+	}
 }
-
-    let reasone = ''
-
-
-function info() {
-
-    let allInfo = []
-    document.querySelectorAll('.info').forEach((key) => {
-        allInfo.push({
-            name: key.id,
-            value: key.value
-        })
-    })
-    // console.log(allInfo)
-
-    const involDt = 'INV RF DT ',
-        doi = 'DOI - ',
-        tkt = 'TKT - ',
-        newTkt = 'NEWTKT - ',
-        emdNum = 'EMD - ',
-        cpns = `CPN TO REF - ${allInfo[32].value}`,
-        reissue = 'REISSUE-0',
-        farePaid = 'FARE PAID',
-        farePaidAdd = 'FARE PAID + ADD PAID',
-        tktPrice = 'TKT PRICE',
-        totRef = 'TOT TO REF',
-        fpCahsCc = 'FP CC + FP CASH',
-        fareUsed = 'FARE USED',
-        taxToRef = 'TAX TO REF',
-        cancelledFltAll = document.querySelector('#cancelledflight').value,
-        cancelledFlt = `${cancelledFltAll.trim().slice(0, 2)}${cancelledFltAll.replace(cancelledFltAll.trim().slice(0, 2), '').replace(cancelledFltAll.trim().slice(-5), '').trim().slice(0, -1).trim()}/${cancelledFltAll.trim().slice(-5)}`
-
-
-    const tktNum = [
-        [],
-        [],
-        [],
-        []
-    ]
-    for (let i = 0; i < allInfo.length; i++) {
-        const el = allInfo[i];
-        if (el.name.slice(0, 6) === 'ticket' && el.value != '') {
-            tktNum[1].push(`${tkt}${el.value.trim()} ${doi}${allInfo[11].value.trim()}`)
-        }
-        if (el.name.slice(0, 15) === 'newticketnumber' && el.value != '') {
-            tktNum[2].push(`${newTkt}${el.value.trim()} ${doi}${allInfo[21].value.trim()}`)
-        }
-        if (el.name.slice(0, 9) === 'emdnumber' && el.value != '') {
-            tktNum[3].push(`${emdNum}${el.value.trim()} ${doi}${allInfo[31].value.trim()}`)
-        }
-    }
-
-    tktNum[0].push(`${involDt}${cancelledFlt} ${allInfo[1].value}`)
-    reasone = `${involDt}${cancelledFlt} ${allInfo[1].value}`
-
-    if ('refund5 refund6 refund7'.indexOf(document.itemsCheck.refundType.value) != -1) {
-        if (tktNum[2].length > 0) {
-            for (let i = 0; i < tktNum[1].length; i++) {
-                const el = tktNum[1][i];
-                tktNum[0].push(tktNum[1][i])
-                tktNum[0].push(tktNum[2][i])
-                tktNum[0].push(`${cpns}`)
-                tktNum[0].push(`Розрахунки ${i + 1} - квитка`)
-            }
-        } else {
-            for (let i = 0; i < tktNum[1].length; i++) {
-                const el = tktNum[1][i];
-                tktNum[0].push(tktNum[1][i])
-                tktNum[0].push(`${cpns}`)
-                tktNum[0].push(`Розрахунки ${i + 1} - квитка`)
-            }
-        }
-    } else if ('refund1 refund2'.indexOf(document.itemsCheck.refundType.value) != -1) {
-        if (tktNum[2].length > 0) {
-            for (let i = 0; i < tktNum[1].length; i++) {
-                const el = tktNum[1][i];
-                tktNum[0].push(tktNum[1][i].trim())
-                tktNum[0].push(tktNum[2][i].trim())
-                tktNum[0].push(`${allInfo[45].value}`)
-            }
-        } else {
-            for (let i = 0; i < tktNum[1].length; i++) {
-                const el = tktNum[1][i];
-                tktNum[0].push(tktNum[1][i])
-                tktNum[0].push(`${allInfo[45].value}`)
-            }
-        }
-    } else if ('refund3 refund4'.indexOf(document.itemsCheck.refundType.value) != -1) {
-        if (tktNum[2].length > 0) {
-            for (let i = 0; i < tktNum[1].length; i++) {
-                const el = tktNum[1][i];
-                tktNum[0].push(tktNum[1][i])
-                tktNum[0].push(tktNum[2][i])
-                tktNum[0].push(`Розрахунки ${i + 1} - квитка`)
-            }
-        } else {
-            for (let i = 0; i < tktNum[1].length; i++) {
-                const el = tktNum[1][i];
-                tktNum[0].push(tktNum[1][i])
-                tktNum[0].push(`Розрахунки ${i + 1} - квитка`)
-            }
-        }
-    }
-
-    if (tktNum[3].length > 0) {
-        for (let i = 0; i < tktNum[3].length; i++) {
-            tktNum[0].push(tktNum[3][i])
-            tktNum[0].push(`${allInfo[45].value}`)
-        }
-    }
-
-
-
-
-    // console.log(tktNum)
-
-    var result = document.querySelector('.result').value = `${tktNum[0].join(' / ')}`
-
-
-}
-
-
-function clearAll() {
-    const inputs = document.querySelectorAll('#ticket input')
-    inputs.forEach(key => key.value = '')
-    const taxesInputs = document.querySelectorAll('.taxes')
-    taxesInputs.forEach(key => key.style.display = 'none')
-
-    const textarea = document.querySelectorAll('ticket textarea')
-    textarea.forEach(key => key.value = '')
-
-    const select = document.querySelectorAll('#ticket select')
-    select.forEach(key => key.value = '')
-
-    const textareaTkt = document.querySelectorAll('#itemsCheck textarea')
-    textareaTkt.forEach(key => key.value = '')
-
-    const textareaInfo = document.querySelectorAll('#ticket textarea')
-    textareaInfo.forEach(key => key.value = '')
-
-    const block = document.querySelectorAll('.refund')
-    block.forEach(key => key.classList.remove('refund-active'))
-    document.getElementsByClassName('cc')[0].style.display = 'none'
-    document.getElementsByClassName('cash')[0].style.display = 'none'
-    document.querySelector('#emdNumList').value = 'Кількість EMD'
-    document.querySelector('#tktNumList').value = 'ticket0'
-    document.querySelector('#refundType').value = 'Оберіть тип повернення'
-}
-
-
-
-
-
-
